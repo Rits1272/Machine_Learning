08b591b22444fc5fae818590dd0982c26f713be6
Ritik Jain <ritikjain1272@gmail.com>
2019-06-13 20:38:32 +0530

    Added Correlation of determination

--- Linear Regression/lr_from_scratch.py
+++ Linear Regression/lr_from_scratch.py
@@ -15,10 +15,20 @@ def best_fit_slope_and_intercept(xs, ys):
 	return m, b
 
 
+def squared_error(ys_orig, ys_line):
+	return sum((ys_orig - ys_line) ** 2)
+
+
+def coeff_of_determination(ys_orig, ys_line):
+	y_mean_line = [mean(ys_orig) for y in ys_orig]
+	squared_error_regr = squared_error(ys_orig, ys_line)
+	squared_error_mean_line = squared_error(ys_orig, y_mean_line)
+	return (1 - (squared_error_regr/squared_error_mean_line))
+
+
 m,b = best_fit_slope_and_intercept(xs, ys)
-
 regression_line = [(m*x) + b for x in xs]
-
+r_squared = coeff_of_determination(ys, regression_line)
 plt.scatter(xs, ys)
 plt.plot(xs, regression_line)
 plt.show()
