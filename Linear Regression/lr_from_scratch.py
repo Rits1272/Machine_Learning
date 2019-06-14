80d2c88cdbce0ba877973cd989d31be05fb6c513
Ritik Jain <ritikjain1272@gmail.com>
2019-06-14 11:22:27 +0530

    Testing assumptions of the model

--- Linear Regression/lr_from_scratch.py
+++ Linear Regression/lr_from_scratch.py
@@ -2,11 +2,22 @@ from statistics import mean
 import numpy as np
 from matplotlib import style
 import matplotlib.pyplot as plt
+import random
 
 style.use('fivethirtyeight')
 
-xs = np.asarray([1,2,3,4,5,6], dtype=np.float64)
-ys = np.asarray([5,4,6,5,6,7], dtype=np.float64)
+#xs = np.asarray([1,2,3,4,5,6], dtype=np.float64)
+#ys = np.asarray([5,4,6,5,6,7], dtype=np.float64)
+
+def create_dataset(hm, step, variance, correlation=False):
+	val = 1
+	ys = []
+	for i in range(hm):
+		y = val + random.randrange(-variance, variance)
+		ys.append(y)
+	xs = [i for i in range(len(ys))]
+	
+	return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 def best_fit_slope_and_intercept(xs, ys):
 	m = ((mean(xs) * mean(ys)) - mean(xs*ys))/((mean(xs)*mean(xs)) - mean(xs*xs))  # PEMDAS
@@ -25,10 +36,15 @@ def coeff_of_determination(ys_orig, ys_line):
 	squared_error_mean_line = squared_error(ys_orig, y_mean_line)
 	return (1 - (squared_error_regr/squared_error_mean_line))
 
-
+xs, ys = create_dataset(40, 2, 40, correlation='pos')
 m,b = best_fit_slope_and_intercept(xs, ys)
 regression_line = [(m*x) + b for x in xs]
 r_squared = coeff_of_determination(ys, regression_line)
+
+predict_xs = 8
+predict_ys = (m*predict_xs) + b
+
 plt.scatter(xs, ys)
+plt.scatter(predict_xs, predict_ys, color='g', s='100')
 plt.plot(xs, regression_line)
 plt.show()
